711ae91655fa8e15e6c6b3f950c73b06308a9446
Austin <Austin.T.Randolph@gmail.com>
2022-01-04 21:17:31 -0500

    Configured snoowrap'

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const Tesseract = require('tesseract.js');
 const Snoowrap = require('snoowrap');
 const Snoostorm = require('snoostorm');
 
+// Build Snoowrap and Snoostorm clients
+const r = new Snoowrap({
+    userAgent: process.env.USER_AGENT,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    username: process.env.REDDIT_USER,
+    password: process.env.REDDIT_PASS
+});
+const client = new Snoostorm(r);
+
 const input = process.argv[2];
 
 const run = function (input) {
