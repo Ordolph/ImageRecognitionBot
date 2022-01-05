6d09240f8e7ab91f118af6fad2d1f100dddcab26
Austin <Austin.T.Randolph@gmail.com>
2022-01-05 15:56:05 -0500

    Added comment stream

--- app.js
+++ app.js
@@ -1,31 +1,43 @@
-require('dotenv').config();
-
-const Tesseract = require('tesseract.js');
-const Snoowrap = require('snoowrap');
-const Snoostorm = require('snoostorm');
+require("dotenv").config();
+const Tesseract = require("tesseract.js");
+const Snoowrap = require("snoowrap");
+const Snoostorm = require("snoostorm");
 
 // Build Snoowrap and Snoostorm clients
 const r = new Snoowrap({
-    userAgent: process.env.USER_AGENT,
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    username: process.env.REDDIT_USER,
-    password: process.env.REDDIT_PASS
+  userAgent: process.env.USER_AGENT,
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  username: process.env.REDDIT_USER,
+  password: process.env.REDDIT_PASS,
 });
-const client = new Snoostorm(r);
 
-const input = process.argv[2];
+const client = new Snoowrap(r);
 
-const run = function (input) {
-    Tesseract.recognize(
-        input,
-        'eng',
-        { logger: m => console.log(m) }
-    ).then(({ data: { text } }) => {
-        console.log(text);
-        return text
-    })
-}
+//Set options for comment stream
+const streamOpts = {
+  subreddit: "WhitePeopleTwitter",
+  results: 25,
+  pollingTime: 2000,
+};
 
-run(input);
+//Create comment stream
+const comments = new Snoostorm.CommentStream(client, streamOpts)
 
+//Print comment on reception
+comments.on("comment", (comment) => {
+  console.log(comment);
+});
+
+// const input = process.argv[2];
+
+// const run = function (input) {
+//   Tesseract.recognize(input, "eng", { logger: (m) => console.log(m) }).then(
+//     ({ data: { text } }) => {
+//       console.log(text);
+//       return text;
+//     }
+//   );
+// };
+
+// run(input);
